185bd8828507b90443347bfec21a70a549cce621
Arsh Goyal <arshgoyalDev@gmail.com>
2024-06-26 21:03:52 +0530

    add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          red: "hsl(0, 100%, 66%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          lightGrayishViolet: "hsl(270, 3%, 87%)",
+          darkGrayishViolet: "hsl(279, 6%, 55%)",
+          veryDarkViolet: "hsl(278, 68%, 11%)",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
